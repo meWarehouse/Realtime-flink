727d48605107f9c5906ab2f8bfba4c22c866ae39
zero <zero@zero.com>
2021-10-13 21:55:26 +0800

    dwd 日志数据分流

--- retime-demo-one/src/main/java/com/at/retime/util/MyKafkaUtil.java
+++ retime-demo-one/src/main/java/com/at/retime/util/MyKafkaUtil.java
@@ -2,8 +2,10 @@ package com.at.retime.util;
 
 import com.at.retime.bean.FlinkKafkaConsumerRecord;
 import com.at.retime.common.FlinkKafkaDeserializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Properties;
@@ -14,6 +16,8 @@ import java.util.UUID;
  */
 public class MyKafkaUtil {
 
+    private static final String BOOTSTRAP_SERVERS = "hadoop102:9092,hadoop103:9092,hadoop104:9093";
+
     private static final FlinkKafkaDeserializationSchema kafkaDeserializationSchema = new FlinkKafkaDeserializationSchema();
 
     public static FlinkKafkaConsumer<FlinkKafkaConsumerRecord> getKafkaConsumer(String topic,String groupId) {
@@ -23,6 +27,10 @@ public class MyKafkaUtil {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop102:9092,hadoop103:9092,hadoop104:9092");
         return new FlinkKafkaConsumer<FlinkKafkaConsumerRecord>(topic, kafkaDeserializationSchema, properties);
 
+    }
+
+    public static FlinkKafkaProducer<String> getKafkaSink(String topic) {
+        return new FlinkKafkaProducer<String>(BOOTSTRAP_SERVERS, topic, new SimpleStringSchema());
     }
 
 }
